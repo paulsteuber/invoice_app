bb303fcbee2534ddb73a7ec697e70288d7d97c40
Paul Steuber <paul.steuber@gmail.com>
2022-02-03 13:18:13 +0100

    welcome and dashboard view changes

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -21,6 +21,7 @@
                             <th class="text-right" scope="col">Netto</th>
                             <th class="text-right" scope="col">MwSt</th>
                             <th class="text-right" scope="col">Brutto</th>
+                            <th scope="col">Status</th>
                             <th scope="col">Aktionen</th>
                         </tr>
                     </thead>
@@ -35,6 +36,7 @@
                             <td class="text-right">{{number_format((float)$invoice->netto_total, 2, ',', '')}}€</td>
                             <td class="text-right"> {{number_format(floatval($invoice->brutto_total) - floatval($invoice->netto_total), 2, ',', '')}}€</td>
                             <th class="text-right" scope="row"> {{number_format((float)$invoice->brutto_total, 2, '.', '')}}€</th>
+                            <th class="text-right" scope="row"> {{$invoice->invoice_state}}</th>
                             <td>
                                 <a type="button" class="pdfDownload">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14 2v11h2.51l-4.51 5.01-4.51-5.01h2.51v-11h4zm2-2h-8v11h-5l9 10 9-10h-5v-11zm3 19v3h-14v-3h-2v5h18v-5h-2z"/></svg>
