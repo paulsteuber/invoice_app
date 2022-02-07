f09f9f835a803fefe39ce0e26554194edd171326
Paul Steuber <paul.steuber@gmail.com>
2022-02-07 12:35:00 +0100

    standard admin user in db users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'App Admin',
+            'email' => 'admin@admin.admin',
+            'password' => bcrypt('admin'),
+        ]);
+    }
+}
